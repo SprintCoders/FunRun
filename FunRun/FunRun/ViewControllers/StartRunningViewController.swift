8f9fcb13a17253f6208a1e45ae48045341422692
kaileding <dingkaile@gmail.com>
2016-11-14 21:27:57 -0800

    skeleton project

---
+++ FunRun/FunRun/ViewControllers/StartRunningViewController.swift
@@ -0,0 +1,43 @@
+//
+//  StartRunningViewController.swift
+//  FunRun
+//
+//  Created by DINGKaile on 11/14/16.
+//  Copyright © 2016 SprintCoders. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class StartRunningViewController: UIViewController {
+
+    @IBOutlet weak var bgMapView: MKMapView!
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    @IBOutlet weak var typeSelector: UISegmentedControl!
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FunRun/FunRun/ViewControllers/TabViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TabViewController.swift
+//  FunRun
+//
+//  Created by DINGKaile on 11/14/16.
+//  Copyright © 2016 SprintCoders. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var tabBarView: UIView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
