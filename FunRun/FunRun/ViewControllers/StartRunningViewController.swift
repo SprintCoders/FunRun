abbafc1c7ce75d6c52fd2280cc55fd42fc85b863
kaileding <dingkaile@gmail.com>
2016-11-16 00:58:19 -0800

    edited start running page

--- FunRun/FunRun/ViewControllers/StartRunningViewController.swift
+++ FunRun/FunRun/ViewControllers/StartRunningViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class StartRunningViewController: UIViewController {
+class StartRunningViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var bgMapView: MKMapView!
     @IBOutlet weak var avatarImage: UIImageView!
@@ -18,10 +18,25 @@ class StartRunningViewController: UIViewController {
     @IBOutlet weak var typeSelector: UISegmentedControl!
     @IBOutlet weak var startButton: UIButton!
     
+    var locationManager: CLLocationManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let side = self.avatarImage.frame.width
+        self.avatarImage.layer.cornerRadius = side/2.0
+        self.avatarImage.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,5 +54,25 @@ class StartRunningViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    /* MARK: - MapView functions */
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            self.locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.1, 0.1)
+            let region = MKCoordinateRegionMake(location.coordinate, span)
+            self.bgMapView.setRegion(region, animated: true)
+        }
+    }
+    
+    
+    
+    
 
 }
