12815bcd7f70eb45d118ae3924ec595e37d462d7
kaileding <dingkaile@gmail.com>
2016-12-05 20:37:35 -0800

    merged with dev/runningview, coredata supported

--- FunRun/FunRun/Services/RunTracker.swift
+++ FunRun/FunRun/Services/RunTracker.swift
@@ -11,7 +11,7 @@ import Foundation
 import CoreLocation
 
 @objc protocol RunTrackerDelegate: class {
-    @objc optional func RunTrackerUpdate(newDistance distance: Double, newAvgPace avgPace: UInt, newSpeed speed: Double)
+    @objc optional func RunTrackerUpdate(newDistance distance: Double, newAvgPace avgPace: UInt, newSpeed speed: Double, newTotalCal calories: Double)
     @objc optional func RunTrackerUpdate(newLocation location: CLLocation?)
 }
 
@@ -31,15 +31,27 @@ class RunTracker: NSObject, CLLocationManagerDelegate {
         didSet {
             if runningStatus == RunningStatus.started {
                 NotificationCenter.default.post(name: NSNotification.Name(rawValue: RunTracker.startedARunningNotification), object: nil)
+                if self.locationManager != nil {
+                    self.locationManager.allowsBackgroundLocationUpdates = true
+                }
+            }else if runningStatus == RunningStatus.paused {
+                if self.locationManager != nil {
+                    self.locationManager.allowsBackgroundLocationUpdates = false
+                }
             } else if runningStatus == RunningStatus.finished {
                 NotificationCenter.default.post(name: NSNotification.Name(rawValue: RunTracker.finishedARunningNotification), object: nil)
+                if self.locationManager != nil {
+                    self.locationManager.allowsBackgroundLocationUpdates = false
+                }
             }
         }
     }
     
     var locations: [CLLocation]?
     var distanceSum: Double = 0.0 // in meters
+    var caloriesSum: Double = 0.0 // in calories
     var timeSum: UInt32 = 0
+    var startTime: Date?
     
     
     override init() {
@@ -50,6 +62,7 @@ class RunTracker: NSObject, CLLocationManagerDelegate {
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
         self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.allowsBackgroundLocationUpdates = false
         
         // Setup variables
         self.runningStatus = RunningStatus.notStart
@@ -64,30 +77,35 @@ class RunTracker: NSObject, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if self.runningStatus == RunningStatus.started {
-            var tempLength: Double = 0.0
-            for alocation in locations {
-                if alocation.horizontalAccuracy < 10 {
-                    // update the record
-                    let lastLocation = self.locations?.last
-                    if lastLocation != nil {
-                        tempLength += alocation.distance(from: lastLocation!)
+        if locations.count > 0 {
+            if self.runningStatus == RunningStatus.started {
+                var tempLength: Double = 0.0
+                for alocation in locations {
+                    if alocation.horizontalAccuracy < 10 {
+                        // update the record
+                        let lastLocation = self.locations?.last
+                        if lastLocation != nil {
+                            tempLength += alocation.distance(from: lastLocation!)
+                        }
+                        self.locations?.append(alocation)
                     }
-                    self.locations?.append(alocation)
                 }
+                self.distanceSum += tempLength
+                var avgPaceInSeconds: Double = 0.0
+                if self.distanceSum > 0 {
+                    avgPaceInSeconds = (Double(self.timeSum) / self.distanceSum) * 1609.344
+                }
+                let lastSpeed: Double = (locations.last?.speed)!
+                if lastSpeed < 1.3 { // walking
+                    self.caloriesSum += 155.0 * 0.53 * tempLength / 1609.344
+                } else { // running
+                    self.caloriesSum += 155.0 * 0.75 * tempLength / 1609.344
+                }
+                print("-- one location update")
+                self.runTrackerDelegate?.RunTrackerUpdate?(newDistance: self.distanceSum, newAvgPace: UInt(avgPaceInSeconds), newSpeed: lastSpeed, newTotalCal: self.caloriesSum)
+            } else if self.runningStatus == RunningStatus.notStart {
+                self.runTrackerDelegate?.RunTrackerUpdate?(newLocation: locations.last)
             }
-            self.distanceSum += tempLength
-            var avgPaceInSeconds: Double = 0.0
-            var avgSpeed: Double = 0.0
-            if self.distanceSum > 0 {
-                avgPaceInSeconds = (Double(self.timeSum) / self.distanceSum) * 1609.344
-            }
-            if avgPaceInSeconds > 0 {
-                avgSpeed = 3600.0 / avgPaceInSeconds
-            }
-            self.runTrackerDelegate?.RunTrackerUpdate?(newDistance: self.distanceSum, newAvgPace: UInt(avgPaceInSeconds), newSpeed: avgSpeed)
-        } else if self.runningStatus == RunningStatus.notStart {
-            self.runTrackerDelegate?.RunTrackerUpdate?(newLocation: locations.last)
         }
     }
     
@@ -95,7 +113,9 @@ class RunTracker: NSObject, CLLocationManagerDelegate {
     func reset() {
         locations?.removeAll()
         distanceSum = 0.0
+        caloriesSum = 0.0
         timeSum = 0
+        startTime = Date()
     }
     
 }
--- FunRun/FunRun/ViewControllers/FinishRunningViewController.swift
+++ FunRun/FunRun/ViewControllers/FinishRunningViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreData
 
-class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, MakeNoteViewControllerDelegate {
+class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, MakeNoteViewControllerDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var runDayLabel: UILabel!
@@ -19,9 +20,10 @@ class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, 
     @IBOutlet weak var distanceViewbox: UIView!
     @IBOutlet weak var durationViewBox: UIView!
     @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var distanceUnitLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var routeMapView: MKMapView!
     
     @IBOutlet weak var avgPaceViewBox: UIView!
     @IBOutlet weak var bestSpeedViewBox: UIView!
@@ -45,8 +47,12 @@ class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, 
     @IBOutlet weak var saveBtn: UIButton!
     @IBOutlet weak var deleteBtn: UIButton!
     
-    var totalTime: UInt32 = 0
     var totalDistance: Double = 0.0
+    var totalTime: UInt32 = 0
+    var avgPace: String = ""
+    var bestSpeed: Double = 0.0
+    var worstSpeed: Double = 0.0
+    var calories: UInt32 = 0
     var chosenFace: UIImageView?
     var tabBarHeight: CGFloat = 0.0
     var notes: String = ""
@@ -60,6 +66,10 @@ class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, 
         self.distanceViewbox.layer.borderColor = UIColor.black.cgColor
         self.durationViewBox.layer.borderWidth = 1.0
         self.durationViewBox.layer.borderColor = UIColor.black.cgColor
+        
+        // Setup Map View
+        self.routeMapView.delegate = self
+        self.routeMapView.showsTraffic = false
         
         // Setup Emoji face images and their container view
         let tapFace1 = UITapGestureRecognizer(target: self, action: #selector(tapEmojiFace(gesture:)))
@@ -95,17 +105,17 @@ class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, 
         self.scheduleViewBox.layer.borderColor = UIColor.black.cgColor
         
         // Setup buttons
-        self.saveBtn.layer.borderWidth = 2.0
-        self.saveBtn.layer.cornerRadius = 5.0
+        // self.saveBtn.layer.borderWidth = 2.0
+        self.saveBtn.layer.cornerRadius = 10.0
         self.saveBtn.clipsToBounds = true
-        let saveBtnClr = self.saveBtn.currentTitleColor
-        self.saveBtn.layer.borderColor = saveBtnClr.cgColor
+        // let saveBtnClr = self.saveBtn.currentTitleColor
+        // self.saveBtn.layer.borderColor = saveBtnClr.cgColor
         self.saveBtn.addTarget(self, action: #selector(saveButtonPressed), for: UIControlEvents.touchUpInside)
-        self.deleteBtn.layer.borderWidth = 2.0
-        self.deleteBtn.layer.cornerRadius = 5.0
+        // self.deleteBtn.layer.borderWidth = 2.0
+        self.deleteBtn.layer.cornerRadius = 10.0
         self.deleteBtn.clipsToBounds = true
-        let deleteBtnClr = self.deleteBtn.currentTitleColor
-        self.deleteBtn.layer.borderColor = deleteBtnClr.cgColor
+        // let deleteBtnClr = self.deleteBtn.currentTitleColor
+        // self.deleteBtn.layer.borderColor = deleteBtnClr.cgColor
         self.deleteBtn.addTarget(self, action: #selector(deleteButtonPressed), for: UIControlEvents.touchUpInside)
         
         // Setup variables
@@ -125,10 +135,20 @@ class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, 
         let dateFormatter = DateFormatter()
         dateFormatter.setLocalizedDateFormatFromTemplate("EEEE")
         self.runDayLabel.text = dateFormatter.string(from: Date()).appending(" Run")
-        // let today = Calendar.current.component(.weekday, from: Date())
-        // self.runDayLabel.text = ""
+        if self.totalDistance/1609.344 < 0.01 {
+            self.distanceLabel.text = String(format: "%.0f", arguments: [self.totalDistance/0.3048])
+            self.distanceUnitLabel.text = "feet"
+        } else {
+            self.distanceLabel.text = String(format: "%.2f", arguments: [self.totalDistance/1609.344])
+            self.distanceUnitLabel.text = "miles"
+        }
         self.durationLabel.text = TimeCount.convertIntToTime(seconds: self.totalTime)
+        self.avgPaceLabel.text = self.avgPace
+        self.bestSpeedLabel.text = String(format: "%.2f", arguments: [self.bestSpeed*2.23694])
+        self.caloriesLabel.text = "\(self.calories)"
         self.notesLabel.text = self.notes
+        
+        self.drawRouteOnMap()
     }
     
     override func didReceiveMemoryWarning() {
@@ -142,12 +162,25 @@ class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, 
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         if segue.identifier == "FinishToNote" {
-            print("-- go to make note")
             let makeNoteVC = segue.destination as! MakeNoteViewController
             makeNoteVC.noteDelegate = self
             makeNoteVC.notes = self.notes
         }
     }
+    
+    
+    /* MARK: - MapView Delegate function */
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay.isKind(of: MulticolorPolylineSegment.classForCoder()) {
+            let colorPolyline = overlay as! MulticolorPolylineSegment
+            let polylineRenderer = MKPolylineRenderer(polyline: colorPolyline)
+            polylineRenderer.strokeColor = colorPolyline.color
+            polylineRenderer.lineWidth = 8.0
+            return polylineRenderer
+        }
+        return MKOverlayPathRenderer(overlay: overlay)
+    }
+    
     
     
     /* MARK: - MakeNoteViewControllerDelegate function */
@@ -167,19 +200,168 @@ class FinishRunningViewController: UIViewController, CLLocationManagerDelegate, 
     }
     
     func saveButtonPressed() {
-        _ = self.navigationController?.popToRootViewController(animated: true)
-        RunTracker.shared.runningStatus = RunningStatus.notStart
+        self.saveRunDataToCoreData {
+            _ = self.navigationController?.popToRootViewController(animated: true)
+            RunTracker.shared.runningStatus = RunningStatus.notStart
+        }
     }
     
     func deleteButtonPressed() {
-        _ = self.navigationController?.popToRootViewController(animated: true)
-        RunTracker.shared.runningStatus = RunningStatus.notStart
+        let deleteAlert = UIAlertController(title: "Delete this run?", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        let confirmAction = UIAlertAction(title: "Delete", style: UIAlertActionStyle.destructive) {(UIAlertAction) in
+            RunTracker.shared.runningStatus = RunningStatus.notStart
+            _ = self.navigationController?.popToRootViewController(animated: true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        deleteAlert.addAction(confirmAction)
+        deleteAlert.addAction(cancelAction)
+        self.present(deleteAlert, animated: true, completion: nil)
     }
 
     @IBAction func tapViewToMakeNote(_ sender: UITapGestureRecognizer) {
         self.performSegue(withIdentifier: "FinishToNote", sender: nil)
     }
     
+    func getMapRegion() -> MKCoordinateRegion {
+        let locationSet = RunTracker.shared.locations
+        var minLatitude: Double = (locationSet?.first?.coordinate.latitude)!
+        var minLongitude: Double = (locationSet?.first?.coordinate.longitude)!
+        var maxLatitude: Double = minLatitude
+        var maxLongitude: Double = minLongitude
+        for gpsXY in locationSet! {
+            if gpsXY.coordinate.latitude < minLatitude {
+                minLatitude = gpsXY.coordinate.latitude
+            }
+            if gpsXY.coordinate.latitude > maxLatitude {
+                maxLatitude = gpsXY.coordinate.latitude
+            }
+            if gpsXY.coordinate.longitude < minLongitude {
+                minLongitude = gpsXY.coordinate.longitude
+            }
+            if gpsXY.coordinate.longitude > maxLongitude {
+                maxLongitude = gpsXY.coordinate.longitude
+            }
+        }
+        let center = CLLocationCoordinate2D(latitude: (minLatitude + maxLatitude)*0.5, longitude: (minLongitude + maxLongitude)*0.5)
+        let centerSpan = MKCoordinateSpan(latitudeDelta: (maxLatitude - minLatitude)*1.2, longitudeDelta: (maxLongitude - minLongitude)*1.2)
+        let fitRegion = MKCoordinateRegion(center: center, span: centerSpan)
+        
+        return fitRegion
+    }
     
+    func getPolyline() -> MKPolyline {
+        var coords = [CLLocationCoordinate2D]()
+        let numOfLocations = RunTracker.shared.locations?.count
+        for location in RunTracker.shared.locations! {
+            coords.append(location.coordinate)
+        }
+        return MKPolyline(coordinates: coords, count: numOfLocations!)
+    }
+    
+    func getColoredPolyline() -> [MulticolorPolylineSegment] {
+        let meanSpeed = (self.bestSpeed + self.worstSpeed) * 0.5
+        let locationSet = RunTracker.shared.locations!
+        var colorSegments = [MulticolorPolylineSegment]()
+        for index in 1...(locationSet.count-1) {
+            let firstLoc = locationSet[index-1]
+            let secondLoc = locationSet[index]
+            var color: UIColor?
+            if secondLoc.speed < meanSpeed {
+                let ratio = (secondLoc.speed - self.worstSpeed) / (meanSpeed - self.worstSpeed)
+                color = ColorGradient.colorBetweenRedAndYellow(ratio: ratio)
+            } else {
+                let ratio = (secondLoc.speed - meanSpeed) / (self.bestSpeed - meanSpeed)
+                color = ColorGradient.colorBetweenYellowAndBlue(ratio: ratio)
+            }
+            let colorSegment = MulticolorPolylineSegment(coordinates: [firstLoc.coordinate, secondLoc.coordinate], count: 2)
+            colorSegment.color = color
+            colorSegments.append(colorSegment)
+        }
+        return colorSegments
+    }
+    
+    func drawRouteOnMap() {
+        if (RunTracker.shared.locations?.count)! > 1 {
+            self.routeMapView.setRegion(self.getMapRegion(), animated: true)
+            // self.routeMapView.addOverlays([self.getPolyline()])
+            self.routeMapView.addOverlays(self.getColoredPolyline())
+        }
+    }
+    
+    /*
+    @IBOutlet var lineFields:[UITextField]!
+    fileprivate let lineEntityName = "Line"
+    fileprivate let lineNumberKey = "lineNumber"
+    fileprivate let lineTextKey = "lineText"
+    */
+    
+    func saveRunDataToCoreData(completion block: (() -> Void)) {
+        do {
+            var locationSet = [Any]()
+            for location in RunTracker.shared.locations! {
+                let oneLocationInfo: [String: Any] = [
+                    "latitude": location.coordinate.latitude,
+                    "longitude": location.coordinate.longitude,
+                    "speed": location.speed,
+                    "time": location.timestamp.description
+                ]
+                locationSet.append(oneLocationInfo as Any)
+            }
+            let jsonObj = ["data": locationSet]
+            let jsonData = try JSONSerialization.data(withJSONObject: jsonObj, options: JSONSerialization.WritingOptions.prettyPrinted)
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let entity = NSEntityDescription.entity(forEntityName: "RunActivity", in: context)
+            let transaction = NSManagedObject(entity: entity!, insertInto: context)
+            
+            transaction.setValue(RunTracker.shared.startTime, forKey: "day")
+            transaction.setValue(self.totalDistance, forKey: "totalDistance")
+            transaction.setValue(self.totalTime, forKey: "totalDuration")
+            transaction.setValue(self.calories, forKey: "totalCalories")
+            transaction.setValue(self.bestSpeed, forKey: "bestSpeed")
+            transaction.setValue(self.worstSpeed, forKey: "worstSpeed")
+            transaction.setValue(jsonData, forKey: "locationSet")
+        
+            try context.save()
+            print("Successfully saved a running data")
+        } catch let error as NSError {
+            print("Failed to save run data to CoreData: \(error.localizedDescription)")
+        } catch {
+            print("Unable to save run data to CoreData")
+        }
+        block()
+    }
+    
+    func retrieveRunDataFromCoreData() {
+        /*
+         
+         //create a fetch request, telling it about the entity
+         let fetchRequest: NSFetchRequest<Transcription> = Transcription.fetchRequest()
+         
+         do {
+         //go get the results
+         let searchResults = try getContext().fetch(fetchRequest)
+         
+         //I like to check the size of the returned results!
+         print ("num of results = \(searchResults.count)")
+         
+         //You need to convert to NSManagedObject to use 'for' loops
+         for trans in searchResults as [NSManagedObject] {
+         //get the Key Value pairs (although there may be a better way to do that...
+         print("\(trans.value(forKey: "audioFileUrlString"))")
+         }
+         } catch {
+         print("Error with request: \(error)")
+         }
+        */
+    }
+    
+    /*
+    func dataFilePath() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        return paths[0].appending("data.sqlite")
+    }
+    */
         
 }
